64f9206278d70dd1b2d04d86f15e3259af01c302
hermeson <hermesondovalebeserra722@gmail.com>
2023-08-21 14:00:05 -0300

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,19 @@ import { GlobalStyle } from './styles/gloabal'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     // permite utilizar a estilização do style components somente dentro da ThemeProvider
+    // provendo os meus defaultThemes com o themeProvider
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
     // themeProvider fica subjulgado a minha variavel que passar dentro do parametro 'theme'
